9753cbb77e8b73f192d4509a222ee11ef70c7f30
Gordon Maloney <gordonmaloney@gmail.com>
2021-09-25 18:36:10 +0100

    edit profile

--- controllers/users.js
+++ controllers/users.js
@@ -48,3 +48,15 @@ export const signup = async (req, res) => {
         res.status(500).json({ message: "Something went wrong."})
     }
 }
+
+
+export const editProfile = async (req, res) => {
+    const { id: _id } = req.params;
+    const post = req.body;
+
+    if(!mongoose.Types.ObjectId.isValid(_id)) return res.status(404).send('No post with that ID');
+    
+    const updatedUser = await user.findByIdAndUpdate(_id, { ...post, _id}, { new: true })
+
+    res.json(updatedPost);
+}
--- routes/users.js
+++ routes/users.js
@@ -1,10 +1,11 @@
 import express from 'express';
 
-import { signin, signup } from '../controllers/users.js'
+import { signin, signup, editProfile } from '../controllers/users.js'
 
 const router = express.Router();
 
 router.post('/signin', signin);
 router.post('/signup', signup);
+router.post('/:user', editProfile)
 
 export default router;
